73c6d7f55d79df7358a280d59c13d934819c1d00
Hiram Cruz <hiram0101@gmail.com>
2017-02-24 15:41:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "super secret session key of the ages"
+end
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+
+    "<h2>#{params[:item]}</h2>"
+  end
+
 end
