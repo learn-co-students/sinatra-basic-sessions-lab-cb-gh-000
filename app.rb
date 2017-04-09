e90e6066fa68714f23952f7a4d91296adcf4373f
Ilya Lavandoso <ultraprolid@gmail.com>
2017-04-09 01:17:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    #binding.pry
+    erb :index
+  end
+
+  post '/checkout' do
+    #binding.pry
+    @session = session
+    @session['item'] = params[:item]
+    "#{@session} #{params[:item]}"
+  end
 end
