0d981f957c3b95650bba0397f71070449a938389
Kacper Kluka <kacper.kluka@gmail.com>
2018-03-18 22:13:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "my_super_duper_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    return session[:item]
+  end
 end
