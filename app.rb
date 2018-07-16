b371e0c789179b27dca18c386659355537b71ec1
Ertugrul Dal <ertugrul.dal@ogr.sakarya.edu.tr>
2018-07-16 19:28:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  get '/checkout' do
+    params.each do |key, value|
+        @session << {key => value}
+      end
+    erb :checkout
+  end
 end
